1fb7bde24ca6232bf94833138e73272944c41533
davi-p-oliveira-11 <dpo.dev11@gmail.com>
2025-05-14 02:23:33 -0300

    cleaned html and added 2 components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { HeroSectionComponent } from './components/hero-section/hero-section.component';
 
 @Component({
   selector: 'app-root',
-  imports: [],
+  imports: [NavbarComponent, HeroSectionComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'halal_lab';
