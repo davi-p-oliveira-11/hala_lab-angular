d95214e34902e71dcda0e2e755cd17d84b9af7df
davi-p-oliveira-11 <dpo.dev11@gmail.com>
2025-05-14 02:51:01 -0300

    updated cta-section

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { NavbarComponent } from './core/navbar/navbar.component';
 
 @Component({
   selector: 'app-root',
-  imports: [],
+  imports: [NavbarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
