bb97e2b373e70042043ac3968082f2e99f5a050d
davi-p-oliveira-11 <dpo.dev11@gmail.com>
2025-05-14 03:52:57 -0300

    added initial skeleton strcuture

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,21 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from './core/navbar/navbar.component';
+import { HeroComponent } from './sections/hero/hero.component';
+import { FeaturesSectionComponent } from './sections/features-section/features-section.component';
+import { FaqSectionComponent } from './shared/faq-section/faq-section.component';
+import { CtaSectionComponent } from './sections/cta-section/cta-section.component';
+import { FooterComponent } from './core/footer/footer.component';
 
 @Component({
   selector: 'app-root',
-  imports: [NavbarComponent],
+  imports: [
+    NavbarComponent,
+    HeroComponent,
+    FeaturesSectionComponent,
+    FaqSectionComponent,
+    CtaSectionComponent,
+    FooterComponent,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
