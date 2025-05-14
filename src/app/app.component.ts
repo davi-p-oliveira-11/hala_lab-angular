791376b444d505398bd29b8c26fc5b1c6554f175
davi-p-oliveira-11 <dpo.dev11@gmail.com>
2025-05-14 02:26:52 -0300

    updated features section

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { HeroSectionComponent } from './components/hero-section/hero-section.component';
+import { FeaturesSectionComponent } from './components/features-section/features-section.component';
 
 @Component({
   selector: 'app-root',
-  imports: [NavbarComponent, HeroSectionComponent],
+  imports: [NavbarComponent, HeroSectionComponent, FeaturesSectionComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
